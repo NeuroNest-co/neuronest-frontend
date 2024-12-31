0efb9181e6908270df1707df91019f3c94f06e5c
Juma Rubea <126375456+jumarubea@users.noreply.github.com>
2024-12-31 19:36:24 +0300

    update Upload.tsx

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import UploadActions from '../components/Upload/UploadActions';
 import ImagePreview from '../components/Upload/ImagePreview';
 import ImageComparison from '../components/Analysis/ImageComparison';
